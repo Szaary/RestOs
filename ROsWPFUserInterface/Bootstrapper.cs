fce4b84f5b9ebd5855c867c04e6fbe9985954acc
Michał Karcz <mk.michalkarcz@gmail.com>
2020-12-20 21:06:30 +0100

    Caliburn setup
    Startup View setup.

---
+++ ROsWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using ROsWPFUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ROsWPFUserInterface
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
