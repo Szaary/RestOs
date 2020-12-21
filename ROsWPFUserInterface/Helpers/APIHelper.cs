db4ff29fc6ce4f3902421c20a87343acb0cfe41b
Michał Karcz <mk.michalkarcz@gmail.com>
2020-12-21 21:57:44 +0100

    Setting up exception, for bad login
    Authenticate user, making WPF show Bad Request message.

--- ROsWPFUserInterface/Helpers/APIHelper.cs
+++ ROsWPFUserInterface/Helpers/APIHelper.cs
@@ -30,7 +30,7 @@ namespace ROsWPFUserInterface.Helpers
         {
             var data = new FormUrlEncodedContent(new[]
             {
-                new KeyValuePair<string, string>("grand_type", "password"),
+                new KeyValuePair<string, string>("grant_type", "password"),
                 new KeyValuePair<string, string>("username", username),
                 new KeyValuePair<string, string>("password", password),
             });
--- ROsWPFUserInterface/ViewModels/LoginViewModel.cs
+++ ROsWPFUserInterface/ViewModels/LoginViewModel.cs
@@ -43,6 +43,35 @@ namespace ROsWPFUserInterface.ViewModels
 			}
 		}
 
+		
+
+		public bool IsErrorVisible
+		{
+			get 
+			{
+				bool output = false;
+				if (ErrorMessage?.Length > 0) output = true;
+				return output; 
+			}			
+		}
+		private string _errorMessage;
+
+		public string ErrorMessage
+		{
+			get { return _errorMessage; }
+			set
+			{ 
+				_errorMessage = value; 
+				NotifyOfPropertyChange(() => IsErrorVisible);
+				NotifyOfPropertyChange(() => ErrorMessage);
+			}
+		}
+
+
+
+
+
+
 
 		public bool CanLogIn
 		{
@@ -61,12 +90,13 @@ namespace ROsWPFUserInterface.ViewModels
 		{
 			try
 			{
+				ErrorMessage = "";
 				var result = await _apiHelper.Authenticate(UserName, Password);
 			}
 			catch (Exception ex)
 			{
 				// Exception if login credidentials are wrong
-				Console.WriteLine(ex.Message);
+				ErrorMessage = ex.Message;
 			}
 
 		}
