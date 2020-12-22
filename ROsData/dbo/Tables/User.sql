8b571f496ebba0ff0cd44284989ad027167e158f
Michał Karcz <mk.michalkarcz@gmail.com>
2020-12-22 01:07:40 +0100

    Connection to Database
    Making of stored procedure to look for logged user.  Debugging Database.

--- ROsData/dbo/Tables/User.sql
+++ ROsData/dbo/Tables/User.sql
@@ -4,7 +4,7 @@
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
     [EmailAddress] NVARCHAR(256) NOT NULL, 
-    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+    [CreatedDate] DATETIME2 NULL DEFAULT getUTCDate()
 
 
 )
---
+++ ROsDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using ROsDataManager.Library.DataAccess;
+using ROsDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Http;
+
+
+namespace ROsDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = HttpContext.Current.User.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId);
+        }
+    }
+}
