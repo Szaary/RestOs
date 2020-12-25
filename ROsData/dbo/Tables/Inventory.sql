65c6c12d914cde81f1c672b825f006d39d94d236
Michał Karcz <mk.michalkarcz@gmail.com>
2020-12-25 16:08:47 +0100

    Add Foreign Key Relationships

--- ROsData/dbo/Tables/Inventory.sql
+++ ROsData/dbo/Tables/Inventory.sql
@@ -4,6 +4,7 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
-    [PurchaseDate] DATETIME2 NULL DEFAULT getUTCdate()
+    [PurchaseDate] DATETIME2 NULL DEFAULT getUTCdate(), 
+    CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
 
 )
