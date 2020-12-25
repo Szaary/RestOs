314514ee95cb91562040e31735b6c97dc56803c9
Michał Karcz <mk.michalkarcz@gmail.com>
2020-12-25 17:57:55 +0100

    Debuging.
    Wrong id in user database.

--- ROsWPFUserInterface/ViewModels/SalesViewModel.cs
+++ ROsWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -131,16 +131,15 @@ namespace ROsWPFUserInterface.ViewModels
 		}
 		private decimal CalculateTax()
 		{
-			decimal TaxAmount = 0;
+			decimal taxAmount = 0;
 			decimal taxRate = _configHelper.GetTaxRate()/100;
-			foreach (var item in Cart)
-			{
-				if (item.Product.IsTaxable)
-				{
-					TaxAmount += item.Product.RetailPrice * item.QuantityInCart * taxRate;
-				}
-			}
-			return TaxAmount;
+
+			taxAmount = Cart
+				.Where(x => x.Product.IsTaxable)
+				.Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
+
+
+			return taxAmount;
 		}
 
 
