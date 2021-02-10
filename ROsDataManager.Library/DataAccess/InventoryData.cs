06e007da4ef222ed5c6f919fea0ae29895096ea2
Michał Karcz <mk.michalkarcz@gmail.com>
2021-02-10 10:03:18 +0100

    Upgrade frone-end library to .NET Standard

--- ROsDataManager.Library/DataAccess/InventoryData.cs
+++ ROsDataManager.Library/DataAccess/InventoryData.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.ModelBinding;
 
 namespace ROsDataManager.Library.DataAccess
 {
