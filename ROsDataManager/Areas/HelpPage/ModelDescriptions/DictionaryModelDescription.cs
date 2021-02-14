41bae6400c07c911530b22205e5f9813dfb83142
Michał Karcz <mk.michalkarcz@gmail.com>
2021-02-14 11:29:20 +0100

    Removed .NetFramework Api

--- ROsDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ROsDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ROsDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ROsDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
